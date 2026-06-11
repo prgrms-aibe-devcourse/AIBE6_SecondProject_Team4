6552e7131d4c40dd3bb5fc6b5d522bae2e0ef6c8
ktk3376 <ktk3376@gmail.com>
2026-06-11 17:49:58 +0900

    feat: RefreshToken 엔티티 및 Repository 추가(미완성)

---
+++ backend/src/main/java/com/fitmate/auth/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package com.fitmate.auth.repository;
+
+import com.fitmate.auth.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(String userId);
+    void deleteByUserId(String userId);
+}
